957d9a81d1af1d85e8db6eb44a2ecb7af63b1871
kn1meR <34110541+kn1meR@users.noreply.github.com>
2025-01-30 16:29:10 -0500

    remove unneeded year select dropdown

--- src/components/TrophyCase.tsx
+++ src/components/TrophyCase.tsx
@@ -49,19 +49,6 @@ const TrophyCase: React.FC = () => {
         <CardHeader className="text-xl font-semibold">
           <div className="flex justify-between items-center">
             <span>Add New Trophy for Year: {selectedYear}</span>
-            <Select
-              value={selectedYear.toString()}
-              onValueChange={(value) => setSelectedYear(parseInt(value))}
-            >
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select Year" />
-              </SelectTrigger>
-              <SelectContent>
-                {Array.from({ length: 5 }, (_, i) => currentYear + i).map(year => (
-                  <SelectItem key={year} value={year.toString()}>{year}</SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
           </div>
         </CardHeader>
         <CardContent>
