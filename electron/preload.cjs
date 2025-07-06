37af4638a1f332c8ddfad19f09f5836001506d50
kn1meR <34110541+kn1meR@users.noreply.github.com>
2025-07-06 16:28:41 -0400

    PURGING v2025

--- electron/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getData: (key) => ipcRenderer.invoke('electron-store-get', key),
-  saveData: (key, value) => ipcRenderer.invoke('electron-store-set', key, value),
-});
