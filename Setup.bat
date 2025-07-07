f408ab0f075319b4bfff57c1c96d76fae69827c2
kn1meR <34110541+kn1meR@users.noreply.github.com>
2025-07-07 11:55:35 -0400

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,8 +1,8 @@
 @echo off
-:: Ask user where the folder is or assume it's on Desktop
 cd /d "%USERPROFILE%\Desktop\CFB-Dynasty-Manager-v2026"
 
 echo Installing dependencies...
 npm install
 
+echo Setup complete. You can now run RunCFBDM.bat to start the app.
 pause
