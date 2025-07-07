efbc62adfaa942f8a061a02390aad352f8332463
kn1meR <34110541+kn1meR@users.noreply.github.com>
2025-07-07 11:47:09 -0400

    Update and rename CFBDM.bat to RunCFBDM.bat

--- CFBDM.bat
+++
@@ -1,7 +0,0 @@
-REM replace folder_path with your actual folder path. ( ex. "C:\Users\kn1meR\Desktop\CFB-Dynasty-Manager-v2026" )
-REM the setup for me would look like this.  replace my folder path with yours!
-REM cd "C:\Users\kn1meR\Desktop\CFB-Dynasty-Manager-v2026"
-REM npm run electron-dev
-
-cd <folder_path>
-npm run electron-dev
---
+++ RunCFBDM.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd /d "%USERPROFILE%\Desktop\CFB-Dynasty-Manager-v2026"
+
+echo Starting app...
+npm run electron-dev
+
+echo When done, press Ctrl+C to stop the app.
+pause
